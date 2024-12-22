6fbdd2b79837481474274ce62f38f193d3102ac6
mirko-geno <mirkogenovesi24@gmail.com>
2024-12-22 18:24:49 -0300

    Fully working CNN

--- programs/vision_model/src/lib.rs
+++ programs/vision_model/src/lib.rs
@@ -3,3 +3,5 @@ pub mod model;
 pub mod data;
 
 pub mod training;
+
+pub mod inference;
--- programs/vision_model/src/main.rs
+++ programs/vision_model/src/main.rs
@@ -1,12 +1,28 @@
-use vision_model::model::ModelConfig;
-use burn::backend::Wgpu;
-
+use vision_model::{model::ModelConfig, training::TrainingConfig};
+use burn::{
+    backend::{Autodiff, Wgpu},
+    optim::AdamConfig,
+    data::dataloader::Dataset,
+};
 
 fn main() {
     type MyBackend = Wgpu<f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyBackend>;
 
-    let device = Default::default();
-    let model = ModelConfig::new(10, 512).init::<MyBackend>(&device);
+    let device = burn::backend::wgpu::WgpuDevice::default();
+    let artifact_dir = "/tmp/model";
 
-    println!("{}", model);
+    vision_model::training::train::<MyAutodiffBackend>(
+        artifact_dir,
+        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+        device.clone(),
+    );
+
+    vision_model::inference::infer::<MyBackend>(
+        artifact_dir,
+        device,
+        burn::data::dataset::vision::MnistDataset::test()
+            .get(42)
+            .unwrap(),
+        );
 }
