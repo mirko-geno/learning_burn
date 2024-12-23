b839b5faf78a552be7b1e54aca1281aa593f6955
mirko-geno <mirkogenovesi24@gmail.com>
2024-12-23 11:42:51 -0300

    Execute modules

--- programs/tensor_basics/src/main.rs
+++ programs/tensor_basics/src/main.rs
@@ -1,22 +1,12 @@
-use burn::{
-    tensor::Tensor,
-    backend::Wgpu
-};
+use burn::backend::{wgpu::WgpuDevice, Wgpu};
 
 // Type alias for the backend to use.
-type Backend = Wgpu;
+// type MyBackend = Wgpu;
 
 fn main() {
-    // Default::default() inherits its type from context
-    let device = Default::default();
+    type MyBackend = Wgpu<f32, i32>;
+    let device = WgpuDevice::default();
 
-    // Creation of two tensors, the first with explicit values and the second
-    // one with ones, with the same shape as the first
-    let tensor_1 = Tensor::<Backend, 2>::from_data([[2., 3.], [4., 5.]], &device);
-    let tensor_2 = Tensor::<Backend, 2>::ones_like(&tensor_1);
-
-    // Print the element-wise addition (done with the WGPU backend) of the two tensors
-    // Using clone method is not recommended, used only for the example
-    println!("Sum:{}", tensor_1.clone() + tensor_2.clone());
-    println!("Sub: {}", tensor_1.clone() - tensor_2.clone());
+    tensor_basics::initialization::initializate::<MyBackend>(device.clone());
+    tensor_basics::ownership::ownership::<MyBackend>(device.clone());
 }
